8e1039186e6f64b174643896bc393ba8a4d18ee6
seek4 <haoyunyangtong@163.com>
2015-07-22 15:58:19 +0800

    start a new hard

---
+++ src/topic/RegularExpressionMatching.java
@@ -0,0 +1,21 @@
+package topic;
+
+/**
+ * https://leetcode.com/problems/regular-expression-matching/
+ * 
+ * @author yangtong
+ *
+ */
+public class RegularExpressionMatching {
+	/**
+	 * 貌似并没有想象中的那么难，理清楚思路再做，注意考虑一些特殊情况
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public boolean isMatch(String s,String p){
+		
+		return false;
+	}
+	
+}
